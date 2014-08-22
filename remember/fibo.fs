305e165538a98b17b51418dd7a02910a8756dff0
Bernard T <bernard.tatin@segula.com>
2014-08-23 01:57:54 +0200

    Le programme FSharp a les mêmes fonctionnalités que le programme SML bien que structurellement, les différences ne sont pas négligeables.

--- remember/fibo.fs
+++ remember/fibo.fs
@@ -26,10 +26,43 @@ let mfibo n =
         | 1 -> 1I
         | n -> if memo.[n] = 0I
                     then
-                        let f = get_fibo (n - 1) + get_fibo(n - 2)
+                        let f = get_fibo (n - 1) + get_fibo (n - 2)
                         memo.[n] <- f
                         f
                     else memo.[n]
+
     get_fibo;;
 
+
+let show_fibo k lafibo=
+    let fk = lafibo k
+    let sfk = string fk
+    printfn "fibo %d = %s" k sfk;;
+
+let rec show_all_fibos n lafibo =
+    match n with
+    | 0 -> show_fibo 0 lafibo
+    | _ ->
+    let _ = show_fibo n lafibo
+    show_all_fibos (n - 1) lafibo;;
+
+let dohelp () =
+    printfn "fibo Number"
+    0
+
+let doit astr =
+    let v = int astr
+    let lafibo = mfibo v
+
+    show_all_fibos v lafibo
+    0
+
+
+[<EntryPoint>]
+let main args =
+    let l = args.Length
+
+    if l = 1
+        then doit args.[0]
+        else dohelp ();;
 

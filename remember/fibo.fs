b9df85aeeae2e81e7484157b1e5b00b707cbae50
Bernard T <bernard.tatin@segula.com>
2014-08-23 02:11:31 +0200

    fibo.fs: La version FSharp se rapproche de la version SML.

--- remember/fibo.fs
+++ remember/fibo.fs
@@ -7,17 +7,6 @@
 
 module fibo
 
-let rec fibo n =
-    match n with
-    | 0 -> 0
-    | 1 -> 1
-    | _ -> fibo (n - 1) + fibo (n - 2);;
-
-let rec fibo2 = function
-    | 0 -> 0
-    | 1 -> 1
-    | n -> fibo2 (n - 1) + fibo2 (n - 2);;
-
 let mfibo n =
     let (memo : bigint array) = Array.zeroCreate (n + 1)
 
@@ -34,28 +23,28 @@ let mfibo n =
     get_fibo;;
 
 
-let show_fibo k lafibo=
-    let fk = lafibo k
-    let sfk = string fk
-    printfn "fibo %d = %s" k sfk;;
-
-let rec show_all_fibos n lafibo =
-    match n with
-    | 0 -> show_fibo 0 lafibo
-    | _ ->
-    let _ = show_fibo n lafibo
-    show_all_fibos (n - 1) lafibo;;
-
 let dohelp () =
-    printfn "fibo Number"
-    0
+    printfn "fibo.exe Number"
+    0;;
 
 let doit astr =
     let v = int astr
     let lafibo = mfibo v
 
-    show_all_fibos v lafibo
-    0
+    let show_fibo k =
+        let fk = lafibo k
+        let sfk = string fk
+        printfn "fibo %d = %s" k sfk
+
+    let rec show_all_fibos n =
+        match n with
+        | 0 -> show_fibo 0
+        | _ ->
+        let _ = show_fibo n
+        show_all_fibos (n - 1)
+
+    show_all_fibos v
+    0;;
 
 
 [<EntryPoint>]

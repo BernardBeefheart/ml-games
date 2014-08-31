b61a6259ea5c64bc2b623d4032700cf74bb73242
U-SEGULA\TATIN_B <TATIN_B@INV26363.segula.local>
2014-08-31 22:14:24 +0200

    la génération du signal s'effectue mais sans déphasage

--- signal/signal-maker.fs
+++ signal/signal-maker.fs
@@ -9,11 +9,42 @@
 
 module signalMaker
 
+type TVoltState =
+    | low = 0
+    | high = 1
+
 type TConfiguration() =
     let mutable signal_frequency:float = 50.0
     let mutable sample_frequency:float = 20000.0
     let mutable phase_diff:float = 0.0
-    let mutable sample_duration:float = 10000.0
+    let mutable sample_duration:float = 10.0
+    let mutable samples = 0
+    let mutable samples_per_period = 0
+    let mutable periods = 0
+
+    let mutable vmin = -5.0
+    let mutable vmax = 5.0
+
+    let show_signal () =
+        let rec show_basic k v =
+            match k with
+            | 0 -> 0
+            | k ->
+                printfn "%8.5f\t%8.5f\t%8.5f" v v v
+                show_basic (k - 1) v
+
+        let show_high samples = show_basic samples vmax
+        let show_low samples = show_basic samples vmin
+
+        let rec iloop kperiod =
+            match kperiod with
+            | 0 -> 0
+            | kperiod ->
+                show_high (samples_per_period / 2)
+                show_low (samples_per_period / 2)
+                iloop (kperiod - 1)
+
+        iloop periods
 
     member this.Setsignal_frequency frequency =
         signal_frequency <- frequency
@@ -43,13 +74,15 @@ type TConfiguration() =
     member this.Getsample_duration =
         sample_duration
 
-    member this.show_me message =
-        printfn "%s:" message
-        printfn "  signal_frequency : %f" signal_frequency
-        printfn "  sample_frequency : %f" sample_frequency
-        printfn "  phase_diff       : %f" phase_diff
-        printfn "  sample_duration  : %f" sample_duration
-        this
+
+    member this.show_me () =
+        samples <- int (sample_frequency * sample_duration)
+        samples_per_period <- int (sample_frequency / signal_frequency)
+        periods <- samples / samples_per_period
+
+        printfn "%8.1f\t%8.1f" sample_frequency sample_duration
+        show_signal ()
+
 
 type TFlag =
     | none = 0
@@ -74,11 +107,10 @@ let main args =
                         | TFlag.phase_diff -> read_args t TFlag.phase_diff (configuration.Setphase_diff (float v))
                         | TFlag.sample_duration -> read_args t TFlag.sample_duration (configuration.Setsample_duration (float v))
                         | TFlag.none -> read_args t TFlag.none config
-        | [] -> config.show_me "Test"
+        | [] -> config.show_me ()
 
 
-    read_args (Array.toList args) TFlag.none configuration
-    0;;
+    read_args (Array.toList args) TFlag.none configuration;;
 
 
 

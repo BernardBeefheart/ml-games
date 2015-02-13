81d0d91569fff834b398443518eb9d032c0c93c0
bernard <bernard.beefheart@outlook.fr>
2015-02-13 12:12:02 +0100

    Ajout de commentaire et code plus cohérent!

--- fsharp/fibo/Src/Help.fs
+++ fsharp/fibo/Src/Help.fs
@@ -8,8 +8,12 @@ module Help
 open Constants
 
 (*
- * dohelp :
+ * dohelp exitValue
  * affiche un texte d'aide et retourne son paramètre
+ * exitValue :  code de sortie du programme. Si exitValue n'est
+ *      pas nul, un message est envoyé sur la console d'erreur
+ *      en fonction de la valeur de ExitValue
+ * return: le paramètre exitValue
  *)
 let dohelp exitValue =
 
@@ -20,7 +24,7 @@ let dohelp exitValue =
         | Constants.ExitErrorNegative -> "ERROR : Negative argument\n"
         | _ -> "ERROR : Unknown error\n"
 
-    eprintf "%s" (firstMessage exitValue)
+    if exitValue <> ExitSuccess then eprintf "%s" (firstMessage exitValue)
     printfn "Usage:"
     printfn "   fibo.exe Number"
     printfn "   where Number is a positive integer"
